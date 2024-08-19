0580d95b3c2dcfc1b062193c1deb65df9f89289e
epic <epic@dev-acelik.hpc4healthlocal>
2024-08-19 13:22:08 -0400

    updated script to take into account errors

--- generate_report.sh
+++ generate_report.sh
@@ -4,31 +4,31 @@ set -oe pipefail
 
 cd /home/epic/chirpp/chirpp
 
+# below is needed to activate conda in an independent script, for some reason this is not available
+# in a non-interactive session
 #source /home/epic/.bashrc
 eval "$(conda shell.bash hook)"
 conda activate chirpp_cpu
 
 files=$(ls .. | grep "Note_Detail")
 
-for file in $files
-do
+for file in $files; do
   echo $file
-  python generate_report.py -c config.yaml -n ../$file -o ../reports/$file.xlsx 2> ../logs/$file.err
-  if [ $? == 0 ]
-  then
+  python generate_report.py -c config.yaml -n ../$file -o ../reports/$file.xlsx 2>../logs/$file.err
+  if [ $? == 0 ]; then
     # move files and encrypt them
     mv ../$file ../processed
     gpg -e -r epic_user ../processed/$file
-    if [ $? == 0 ]
-    then
-      rm ../processed/file
+    if [ $? == 0 ]; then
+      rm ../processed/$file
     fi
-
     gpg -e -r epic_user ../reports/$file.xlsx
-    if [ $? == 0 ]
-    then
+    if [ $? == 0 ]; then
       rm ../reports/$file.xlsx
     fi
     echo "$file done"
+  else
+    mv ../$file ../error
+    gpg -e -r epic_user ../error/$file
   fi
 done
