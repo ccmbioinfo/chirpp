5a59cd9b46fe57f6a433ef220fc274188456b1df
alper <celikalper84@gmail.com>
2024-09-16 12:18:02 -0400

    moved some files around, fixed typo in setup.py for pip installation

---
+++ chirpp/deamon.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+cd ~
+
+while true
+do
+  if [[ $(ls | grep "txt") ]]
+  then
+    chirpp/chirpp/generate_report.sh
+  else
+    sleep 1h
+  fi
+done
+
+
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author='Alper Celik',
     author_email='alper.celik@sickkids.ca',
     packages=find_packages(),
-    install_requires=["pandas", "spacy", "medspacy", "spacy_transformers", "transformers", "torch", "tokenizers"
+    install_requires=["pandas", "spacy", "medspacy", "spacy_transformers", "transformers", "torch", "tokenizers",
                       "sentence-transformers", "Levenshtein"],
     zip_safe=False,
     scripts=["chirpp/generate_report.py"],
