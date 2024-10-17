624375778627075609850c9adef2ff92dbf7e35c
alper <celikalper84@gmail.com>
2024-10-17 09:59:04 -0400

    current placeholders for processing reserach events and am also planning to move autofill (things that are not determined by fine-tuned models) to its own class, this is an empty placeholder for now and is not used in other codes

---
+++ chirpp/postprocess/autofill.py
@@ -0,0 +1,36 @@
+import os
+import subprocess
+import sys
+
+import medspacy
+import pandas as pd
+import spacy
+from medspacy.context import ConText
+
+from chirpp.postprocess.extra_contex_rules import context_rules
+from chirpp.postprocess.target_rules import *
+
+from utils import *
+
+
+class Autofill:
+    def __init__(self, raw_notes, inference_notes):
+        pass
+
+    def fill_substances(self):
+        pass
+
+    def fill_dispositions(self):
+        pass
+
+    def fill_bp_no(self):
+        pass
+
+    def fill_sd(self):
+        pass
+
+    def fill_notes(self):
+        pass
+
+    def touchups(self):
+        pass
