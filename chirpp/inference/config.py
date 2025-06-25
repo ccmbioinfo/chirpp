b5592856cfb945fa7d8cfe598628f1ace46c9ab6
alper <celikalper84@gmail.com>
2025-06-25 13:37:00 -0400

    separating the main config into different python configs

---
+++ chirpp/inference/config.py
@@ -0,0 +1,106 @@
+from chirpp.inference.prompts import *
+
+inference={
+    "pipelines": {
+        "classification": {
+         "num_labels": 2,
+        "include_labels": False,
+        "max_length": 128,
+        "truncation": True,
+        "cutoff": 0.95,
+        "classification_model": "./models/distilbert"
+        },
+        "intent": {
+            "num_labels": 7,
+            "include_labels": False,
+            "max_length": 128,
+            "truncation": True,
+            "cutoff": 0.8,
+            "intent_model": "./models/albert_xxl_intent",
+            "labels": { '0': '10', '1': '11', '2': '12', '3': '13', '4': '15', '5': '16', '6': '19' }
+        },
+        #TODO this is not done
+        "chunking": {
+        },
+        #TODO this is not complete
+        "embedding": {
+            "model": "models/m2v_model",
+            "embedding_model": "models/m2v_model"
+        },
+    },
+    "server": {
+        "system_prompt": system_prompt,
+        "summarization": {
+            "model": "models/summary.gguf",
+            "prompt": summary_prompt,
+            "max_tokens": 60,
+            "temperature": 0.7,
+        },
+        "location": {
+            "model": "models/location.gguf",
+            "prompt": location_prompt,
+            "max_tokens": 15,
+            "temperature": 0.1,
+        },
+        "area": {
+            "model": "models/area.gguf",
+            "prompt": area_prompt,
+            "max_tokens": 15,
+            "temperature": 0.1,
+        },
+        "substance": {
+            "model": "models/substance.gguf",
+            "prompt": substance_prompt,
+            "max_tokens": 90,
+            "temperature": 0.4,
+        },
+        "am_pm": {
+            "model": "models/am_pm.gguf",
+            "prompt": ampm_prompt,
+            "max_tokens": 15,
+            "temperature": 0.1,
+        },
+        "io": {
+            "model": "models/io.gguf",
+            "prompt": io_prompt,
+            "max_tokens": 15,
+            "temperature": 0.1,
+        },
+        "sports": {
+            "model": "models/sports.gguf",
+            "prompt": sports_prompt,
+            "max_tokens": 15,
+            "temperature": 0.1,
+        },
+        "time": {
+            "model": "models/time.gguf",
+            "prompt": time_prompt,
+            "max_tokens": 15,
+            "temperature": 0.1,
+        },
+        "safety": {
+            "model": "models/safety.gguf",
+            "prompt": safety_prompt,
+            "max_tokens": 90,
+            "temperature": 0.4,
+        },
+    },
+  "pos_complaints": [ "Oral / Esophageal Foreign Body", "Upper Extremity Injury", "Lower Extremity Injury",
+                    "Burn", "Anxiety / Situational Crisis", "Head Injury", "Medical Device Problem",
+                    "Depression / Suicidal / Deliberate Self Harm", "Paediatric Disruptive Behaviour",
+                    "Laceration/Puncture", "Abrasion", "Neck Trauma", "Noxious Inhalation", "Foreign Body, Nose",
+                    "Post-operative Complications", "Overdose Ingestion", "Bizarre Behaviour", "Cast Check",
+                    "Foreign Body Ear", "Major Trauma - Penetrating", "Eye Trauma", "Concern For Patient Welfare",
+                    "Suture / Staple Removal", "Major Trauma - Blunt", "Foreign Body, Skin", "Violent / Homicidal Behaviour",
+                    "Sexual Assault", "Substance Misuse / Intoxication", "Hypothermia", "Traumatic Back / Spine Injury",
+                    "Nasal Trauma", "Foreign Body, Eye", "Respiratory Foreign Body", "Bite", "Facial Trauma",
+                    "Chemical Exposure, Eye", "Genital Trauma", "Amputation", "Social Problem", "Hallucinations / Delusions",
+                    "Insomnia", "Ear Injury", "Chemical Exposure", "Foreign Body in Rectum", "Isolated Chest Trauma - Blunt",
+                    "Isolated Abdominal Trauma - Blunt", "Anal / Rectal Trauma", "Multisystem Trauma - Blunt", "Foreign Body, Vagina",
+                    "Body Fluid Exposure", "Ring Removal", "Multisystem Trauma - Penetrating", "Near Drowning", "Isolated Abdominal Trauma - Penetrating",
+                    "Isolated Chest Trauma - Penetrating", "Substance Withdrawal", "Electrical Injury", "Frostbite / Cold Injury",
+                    "Cardiac Arrest (Traumatic)", "Foreign Body in Eye" ],
+
+  "final_columns": [ "MRN", "Arrival Date", "Note Text", "probs", "summary", "cosine_similarity", "Diagnosis",
+                   "Problem List", "Chief Complaint" ]
+}
---
+++ chirpp/postprocess/config.py
@@ -0,0 +1,21 @@
+post_process={
+  "cc_filter": [ 'depression / suicidal / deliberate self harm', 'ingestion', 'chemical', 'burn',
+               'concern for patient wellfare', 'overdose ingestion', 'substance misuse / intoxication',
+               'vomiting and/or nausea', 'behaviour', 'altered level Of consciousness' ],
+
+  "diag_pl_filter": [ 'ingestion', 'poisoning', 'overdose', 'exposure', 'anxiety', 'substance',
+                    'intoxication', 'chemical', 'depression', 'suicidal','depressive', 'crisis',
+                    'instability', 'burn', 'self-harm', 'vomitting', 'irritation', 'suicide',
+                    'depressed', 'anorexia nervosa', 'caustic', 'corrosive' ],
+
+
+  "report_header": [ "CSN", "MRN", "ScrMRN", "DOB", "AGE", "SEX", "POSTAL", "ER Time", "ER Date", "ER Day", "INJ DATE", "Hr", "Min", "AM/PM",
+                   "I/O", "LOCATION", "AREA", "PLACE", "Diagnosis", "SK Narrative", "PHAC Narrative", "W4P", "NO1", "BP1", "NO2",
+                   "BP2", "NO3", "BP3", 'veh', 'veh p',"Notes", 'LOS', "DISP", "IN", "sub", "subID", 'sd1', "sd2", "sd3", "sd4", "sd5", "SPORTS CODE",
+                   "E1", "E2", "E3", "E4", "CTAS", "Chief Complaint", "Problem List", "probs", "pre_processed", "Disposition"],
+
+  "note_types": [ 'ED Triage Notes', 'ED Provider Notes', 'PS Initial Consult', 'Consults',
+                'Consult Follow Up', 'Admission', 'Discharge Summary', 'Progress Notes',
+                'Assessment & Plan Note',  'ED Trauma Note', 'ED Trauma Notes',
+                'ED Procedure Note', 'Procedures','Op Note', 'Transfer In / Advice Note' ],
+}
